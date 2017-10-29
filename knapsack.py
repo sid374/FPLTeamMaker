bc274c4a4a81e923d718a3b02979bfe32090b4e9
sid374 <bidasar2@illinois.edu>
2017-10-28 23:37:33 -0400

    Rewrote basic DP function

--- knapsack.py
+++ knapsack.py
@@ -9,9 +9,10 @@ class Team:
 		pass
 
 def main():
-	players = AssignPoints(count=6)
+	players = AssignPoints(count=100)
 	#PrintPlayerList(players)
 	print knapsack_saveTeam_recursive(len(players)-1, 1000, players, [])
+	#print basicDP(players, 1000)
 	PrintPlayerList(finalSelected)
 
 def basicKnapsack_recursive(index, budget, players):
@@ -30,6 +31,37 @@ def basicKnapsack_recursive(index, budget, players):
 				players[index].rating + basicKnapsack_recursive(index-1, budget-players[index].cost, players),
 				basicKnapsack_recursive(index-1, budget, players )
 			)
+
+def basicDP(players, budget):
+	cache = [[0 for x in range(budget+1)] for x in range(len(players) + 1)]
+	for player in xrange(len(players)+1):
+		for cost in xrange(budget+1):
+			if player == 0 or cost == 0:
+				continue
+			if players[player-1].cost > cost:
+				#print cost
+				cache[player][cost] = cache[player-1][cost]
+			else:
+				cache[player][cost] = max( players[player-1].rating + cache[player-1][cost-players[player-1].cost],
+										 cache[player-1][cost] )
+
+	#walk thorugh the cache to find the selected players
+	selectedList = []
+	player, cost = len(players), budget
+	while player >= 0 and cost >= 0:
+		if cost - players[player-1].cost > 0 and cache[player][cost] - players[player-1].rating == cache[player-1][cost - players[player-1].cost]:
+			selectedList.append(players[player-1])
+			cost = cost - players[player-1].cost
+			player = player - 1	
+		else:
+			player = player - 1
+
+	#Update global
+	global finalSelected
+	global finalRating
+	finalSelected = selectedList[:]
+	finalRating  = GetPlayerListRating(finalSelected)
+	return cache[len(players)][budget]
 
 def knapsack_saveTeam_recursive(index, budget, players, selectedPlayers):
 	'''
